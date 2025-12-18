1457ba7d22aa41ed7f6bbae484d3caf0ce15f86e
kavyaa1301 <kavyajain2808@gmail.com>
2025-12-18 17:57:56 +0530

    Add files via upload

---
+++ # password strenght checker.py
@@ -0,0 +1,41 @@
+# password strenght checker
+
+import re
+
+#password strength check conditions:
+# min 8 chars, digit ,uppercase , lowercase & special chars
+
+def check_password_strength(password):
+    if len(password) < 8 :
+        return "Weak : password must be at least 8 chars"
+    
+    if not any(char.isdigit() for char in password):
+        return "Weak : password must contain a digit"
+    
+    if not any(char.isupper()for char in password):
+         return "Weak : password must contain upper char"
+
+    if not any(char.islower()for char in password):
+         return "Weak : password must contain lower char"
+    
+    if not re.search(r'[!@#$%^&*({<>.?})]',password):
+        return "Medium : password must contain a special char"
+    
+    return "Strong : Your password is secured!"
+
+def password_checker():
+    print ("Welcome to the password strenght checker")
+
+    while True :
+        password = input("Enter your password (or type 'exit' to quit):")
+
+        if password.lower() == 'exit' : 
+            print ("Thank you for using this tool")
+            break
+        result = check_password_strength(password)
+        print(result)
+
+
+# Run the password checker tool 
+if __name__ == "__main__":
+    password_checker()
